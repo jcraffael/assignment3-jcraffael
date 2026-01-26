45788683054c26d547abe8c532b308ffb5f4be33
vboxuser <vboxuser@UbuntuClient.station>
2026-01-26 15:15:24 +0000

    Minor correction

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 #
 
-if [[ $# < 2 ]]; then
+if [ $# -lt 2 ]; then
 	echo "The searching path and the searching string are needed"
       	exit 1
-elif [ ! -d $1 ]; then
+elif [ ! -d "$1" ]; then
        echo "The searching path is not existing"
 	exit 1
 fi	
 
-NUM_FILES=$(find $1 -type f | wc -l)
-NUM_MATCH_LINES=$(grep -rnw $1 -e $2 | wc -l)
+NUM_FILES=$(find "$1" -type f | wc -l)
+NUM_MATCH_LINES=$(grep -rnw "$1" -e "$2" | wc -l)
 
 echo "The number of files are $NUM_FILES and the number of matching lines are $NUM_MATCH_LINES"
